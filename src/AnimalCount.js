5e3c4229f7b1e99e267768ea29b294d96fab15ce
Yocanantham <yocanantham@gmail.com>
2024-08-03 14:10:44 +0530

    feat : added heart

---
+++ src/AnimalCount.js
@@ -0,0 +1,49 @@
+import "./AnimalCount.css";
+import { useState } from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/bird.svg";
+import gator from "./svg/gator.svg";
+import horse from "./svg/horse.svg";
+import heart from "./svg/heart.svg";
+
+function AnimalCount({ type }) {
+    // -> Required.
+    const svgMap = {
+        bird,
+        cat,
+        cow,
+        dog,
+        gator,
+        horse,
+    };
+
+    // -> State Handler.
+    const [count, setCount] = useState(0);
+
+    // -> Event handler.
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div onClick={handleClick} className="animal_card">
+            <img
+                className="animal_image"
+                src={svgMap[type]}
+                alt="animal"
+                title={type}
+            />
+            <img
+                className="heart"
+                src={heart}
+                alt="heart"
+                title="heart"
+                style={{ width: 15 + count * 10 + "px" }}
+            />
+        </div>
+    );
+}
+
+export default AnimalCount;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AnimalCount from "./AnimalCount";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // -> Required.
+    const AnimalsList = ["bird", "cat", "cow", "dog", "gator", "horse"];
+    function getRandomAnimal() {
+        return AnimalsList[Math.floor(Math.random() * AnimalsList.length)];
+    }
+
+    // -> State Handler.
+    const [Random, setAnimal] = useState([]);
+
+    // -> Event Handler.
+    const handleClick = () => {
+        setAnimal([...Random, getRandomAnimal()]);
+    };
+
+    // -> Making a map.
+    const AnimalDisplay = Random.map((animal, index) => {
+        return <AnimalCount type={animal} key={index} />;
+    });
+
+    return (
+        <div className="main_wrapper">
+            <h1 className="header">Event and State Learning Example</h1>
+            <button className="button" onClick={handleClick}>
+                Add Animal
+            </button>
+            <div className="main">{AnimalDisplay}</div>
+        </div>
+    );
 }
 
 export default App;
