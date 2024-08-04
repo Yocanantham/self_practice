232c811ff8d0837035cf8129c4e98a1c4afd279e
Yocanantham <yocanantham@gmail.com>
2024-08-05 00:41:37 +0530

    feat : addded additional styles for enhancing

--- src/AnimalCount.js
+++ src/AnimalCount.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import bird from "./svg/bird.svg";
 import cat from "./svg/cat.svg";
 import cow from "./svg/cow.svg";
-import dog from "./svg/bird.svg";
+import dog from "./svg/dog.svg";
 import gator from "./svg/gator.svg";
 import horse from "./svg/horse.svg";
 import heart from "./svg/heart.svg";
